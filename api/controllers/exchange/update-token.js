3766cb0dd1eb985de2fe6b5a8210b39e01465cb6
Stuart Kushner <stuartbkushner@gmail.com>
2019-11-19 12:51:01 -0500

    Initial commit to Project Page

--- api/controllers/exchange/update-token.js
+++ api/controllers/exchange/update-token.js
@@ -9,10 +9,10 @@ module.exports = {
 
   inputs: {
     tokenId: {
-        description: 'Id of the token',
-        example: '10',
-        required: true
-      },
+      description: 'Id of the token',
+      example: '10',
+      required: true
+    },
     name: {
       description: 'name paid to token holdrs',
       example: '10',
@@ -53,36 +53,36 @@ module.exports = {
     var userId = this.req.me.id;
     var token = await sails.models.token.findOne({id:tokenId});
     var current_frequency = token.symbol;
-    if(token.userId == userId){
+    if(token.userId === userId){
       var images = await sails.helpers.files.upload.with({
         req: this.req,
         // folder:"tokens",
         type:'logo'
       });
-        await sails.models.token.update({ id: tokenId})
+      await sails.models.token.update({ id: tokenId})
         .set({
-            name: name,
-            symbol: symbol,
-            description: description,
-            twitter: twitter,
-            website: website,
+          name: name,
+          symbol: symbol,
+          description: description,
+          twitter: twitter,
+          website: website,
         });
-        
-        console.log('token should e set');
-        token = await sails.models.token.findOne({id:tokenId});
-        return token;
 
-        
+      console.log('token should e set');
+      token = await sails.models.token.findOne({id:tokenId});
+      return token;
+
+
     }else{
-        console.log('not authorized',token.userId,user_id);
-        return false;
+      console.log('not authorized',token.userId, userId);
+      return false;
     }
-    
+
     // // Hash the new token.
     // var hashed = await sails.helpers.tokens.hashPassword(inputs.token);
 
     // // Update the record for the logged-in user.
-   
+
 
   }
 
