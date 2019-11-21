dc234f1d3e75d584cfdc6d7631d9f6ff0a94752a
Stuart Kushner <stuartbkushner@gmail.com>
2019-11-21 10:55:45 -0500

    View-project page javascript implemented

--- api/controllers/dashboard/view-project.js
+++ api/controllers/dashboard/view-project.js
@@ -21,9 +21,21 @@ module.exports = {
     // Respond with view.
     var project_id = this.req.param('project_id');
     console.log(project_id);
+    var grids;
     // if project exists, grab the grids
-
-    return {};
+    if (project_id) {
+      /*global Project, a*/
+      grids = await Project.find().populate('grids');
+      console.log(grids);
+      // else, just return an empty object
+    } else {
+      grids = {};
+      console.log(grids);
+    }
+    return {
+      grids:grids,
+      project_id:project_id
+    };
 
   }
 
